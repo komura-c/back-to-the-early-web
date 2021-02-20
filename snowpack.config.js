4c381d5e7ee3ba533f52f0911371b0567569f209
komura <affirman03139@outlook.jp>
2021-02-20 21:53:42 +0900

    add pulugins

--- snowpack.config.js
+++ snowpack.config.js
@@ -8,14 +8,20 @@ module.exports = {
     '@snowpack/plugin-svelte',
     '@snowpack/plugin-dotenv',
     '@snowpack/plugin-typescript',
+    ['@snowpack/plugin-sass', 
+      { /*オプション設定*/ }
+    ],
+    ['@snowpack/plugin-optimize',
+      { /*オプション設定*/ }
+    ],
   ],
   routes: [
     /* Enable an SPA Fallback in development: */
-    // {"match": "routes", "src": ".*", "dest": "/index.html"},
+    {"match": "routes", "src": ".*", "dest": "/index.html"},
   ],
   optimize: {
     /* Example: Bundle your final build: */
-    // "bundle": true,
+    "bundle": true,
   },
   packageOptions: {
     /* ... */
